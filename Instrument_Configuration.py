45fe5054ed4a0a975e11247aeb75c65e07e6d69c
Luis <luisalejandrochh@gmail.com>
2022-12-14 07:42:48 -0500

    Instrument_Configuration modificado

--- Instrument_Configuration.py
+++ Instrument_Configuration.py
@@ -1,2 +1,6 @@
 import pyvisa
-def configuration():
+rm = pyvisa.ResourceManager()
+rm.list_resources()
+inst = rm.open_resource('ASRL/dev/ttyUSB0::INSTR')
+inst.write_termination = "\n"
+inst.read_termination = "\n"
